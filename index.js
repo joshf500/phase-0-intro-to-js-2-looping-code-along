1d42709d7d3a803623a0d51021f520c4091b5acd
Josh Feldman <joshfeldman500@gmail.com>
2023-03-12 05:00:56 -0400

    loops lab complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const cards = []
+function writeCards(name, event){
+    for (let i = 0; i < name.length; i++) {
+        cards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+}
+return cards;
+}
+function countDown(count) {
+    while (count>=0){
+        console.log(count--)
+    }
+}
+
